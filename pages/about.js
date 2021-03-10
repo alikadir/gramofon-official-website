da08ffe36c90c13e8d50c64846c22295901a8829
Ali Kadir Bagcioglu <alikadirbagcioglu@gmail.com>
2021-03-10 19:55:44 +0300

    the project has been converted to nextjs project

---
+++ pages/about.js
@@ -0,0 +1,26 @@
+import Head from 'next/head';
+
+function About(props) {
+  return (
+    <div>
+      <Head>
+        <title>Gramofon - About</title>
+      </Head>
+      <p>
+        With a power of 22 years' experience in the automotive industry, we will develop a huge digital
+        ecosystem for the automotive industry called Gramofon via modern technology.
+      </p>
+      <p>
+        render time: <b>{props.date}</b>
+      </p>
+      <p>
+        render time (utc): <b>{props.dateUtc}</b>
+      </p>
+    </div>
+  );
+}
+export async function getStaticProps() {
+  return { props: { dateUtc: new Date().toUTCString(), date: new Date().toString() } };
+}
+
+export default About;
