8062a659e2782ceb588d4f67481430b197e69c7c
Ali Kadir Bagcioglu <alikadirbagcioglu@gmail.com>
2021-04-14 15:06:04 +0300

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -21,7 +21,7 @@ function About(props) {
   );
 }
 export async function getStaticProps() {
-  return { props: { dateUtc: new Date().toUTCString(), date: new Date().toLocaleDateString('tr-TR') } };
+  return { props: { dateUtc: new Date().toUTCString(), date: new Date().toLocaleString('tr-TR') } };
 }
 
 export default About;
