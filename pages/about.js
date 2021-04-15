35008441c83381fdfad74fa5729954dfbb438366
Ali Kadir Bagcioglu <alikadirbagcioglu@gmail.com>
2021-04-15 13:42:26 +0300

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -9,7 +9,7 @@ function About(props) {
       <h2>Gramofon</h2>
       <p>
         With a power of 22 years' experience in the automotive industry, we will develop a huge digital
-        ecosystem for the automotive industry called Gramofon via modern technology.
+        ecosystem for the automotive industry called Gramofon via modern technology...
       </p>
       <p>
         current time: <b>{new Date().toString()}</b>
